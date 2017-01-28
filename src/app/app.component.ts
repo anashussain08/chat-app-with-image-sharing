bd71c2f2c77b27d0fd35e8ff965e87fad4ccdd1f
anashussain08 <anas.hussain08@gmail.com>
2017-01-28 21:43:16 +0500

    fixed db bugs

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'app works!';
+  constructor(public authService:AuthService, public router:Router){
+
+  }
+  isLoggedin(){
+    return !this.authService.User;
+  }
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
